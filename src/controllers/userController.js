53457a6462f46434d7bebdbe883869acdaa5ea84
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-08-01 02:56:54 +0500

    add twilio

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,70 @@
 const User = require("../models/userModel");
+const twilio = require("twilio");
 const { sendOtpEmail, sendOtp } = require("../utils/sendOtp");
 
+require("dotenv").config();
+
+const client = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
 function generateOtp() {
-  return Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit
+  return Math.floor(100000 + Math.random() * 900000).toString();
 }
+
+exports.sendOtpToMobile = async (req, res) => {
+  const { fullName, email, phoneNumber, password, termsAccepted, location } =
+    req.body;
+
+  if (!termsAccepted) return res.status(400).json({ message: "Accept terms." });
+  if (!phoneNumber || !email)
+    return res.status(400).json({ message: "Phone or email missing." });
+
+  // Check existing user by email or phone
+  const existingUser = await User.findOne({
+    $or: [{ email }, { phoneNumber }],
+  });
+  if (existingUser)
+    return res
+      .status(409)
+      .json({ message: "User already exists with this email or phone." });
+
+  const otp = generateOtp();
+  const otpExpires = new Date(Date.now() + 5 * 60000); // 5 minutes
+
+  try {
+    // await client.messages.create({
+    //   body: `Your OTP code is ${otp}. It will expire in 5 minutes.`,
+    //   messagingServiceSid: "MGc3865ad9835efadc04342cd4715fd4bb",
+    //   to: "+18777804236",
+    // });
+    await client.messages.create({
+      body: `Your OTP code is ${otp}. It will expire in 5 minutes.`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: phoneNumber,
+    });
+
+    const newUser = await User.create({
+      fullName,
+      email,
+      phoneNumber,
+      password,
+      termsAccepted,
+      location,
+      otp,
+      otpExpires,
+      isVerified: false,
+    });
+
+    res.status(201).json({ message: "OTP sent to phone", userId: newUser._id });
+  } catch (error) {
+    console.error("❌ Error sending OTP:", error.message);
+    res
+      .status(500)
+      .json({ message: "Failed to send OTP", error: error.message });
+  }
+};
 
 exports.registerUser = async (req, res) => {
   const { fullName, email, phoneNumber, password, termsAccepted, location } =
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, verifyOtp } = require("../controllers/userController");
+const {
+  registerUser,
+  verifyOtp,
+  sendOtpToMobile,
+} = require("../controllers/userController");
 
 router.post("/register", registerUser);
 router.post("/verify-otp", verifyOtp);
+router.post("/send-otp/mobile", sendOtpToMobile);
 
 module.exports = router;
